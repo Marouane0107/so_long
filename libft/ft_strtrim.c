be52ac12dbdfca1ccc274d493bc63f5c6fba8aba
Marouane Aouzal <maouzal@e1r6p2.1337.ma>
2022-10-16 19:14:33 +0100

    trim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,21 +6,69 @@
 /*   By: maouzal <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 19:17:18 by maouzal           #+#    #+#             */
-/*   Updated: 2022/10/14 21:19:16 by maouzal          ###   ########.fr       */
+/*   Updated: 2022/10/16 19:10:48 by maouzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft"
+#include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+int	ft_start(char const *s1, char const *set)
 {
 	unsigned int	x;
 	unsigned int	i;
 
 	x = 0;
 	i = 0;
-	while (set[i] != '\0' && s1[x] != '\0')
+	while (s1[x] != '\0' && set[i] != '\0')
 	{
 		if (s1[x] == set[i])
+		{
+			x++;
+			i = 0;
+			continue ;
+		}
+		i++;
 	}
+	return (x);
+}
+
+int	ft_end(char const *s1, char const *set)
+{
+	unsigned int	x2;
+	unsigned int	i;
+
+	i = 0;
+	x2 = ft_strlen(s1) - 1;
+	while (x2 > 0 && set[i] != '\0')
+	{
+		if (s1[x2] == set[i])
+		{
+			x2--;
+			i = 0;
+			continue ;
+		}
+		i++;
+	}
+	return (x2);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char			*s2;
+	unsigned int	i;
+	unsigned int	x;
+	unsigned int	x2;
+
+	i = 0;
+	x = ft_start(s1, set);
+	x2 = ft_end(s1, set);
+	s2 = malloc((x2 - x) + 2);
+	while (x <= x2)
+	{
+		s2[i] = s1[x];
+		i++;
+		x++;
+	}
+	s2[i] = '\0';
+	return (s2);
 }
