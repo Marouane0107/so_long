bef6e60b73ce4f19f770e6e162ede553625902de
Marouane Aouzal <maouzal@e1r4p6.1337.ma>
2022-10-23 09:10:50 +0100

    Makefile

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,11 +6,12 @@
 /*   By: maouzal <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 04:31:16 by maouzal           #+#    #+#             */
-/*   Updated: 2022/10/13 13:11:37 by maouzal          ###   ########.fr       */
+/*   Updated: 2022/10/22 08:43:17 by maouzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 void	*ft_memmove(void	*dest, const void	*src, size_t	len)
 {
